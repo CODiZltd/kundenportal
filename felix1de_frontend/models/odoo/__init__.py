d73a34efe2e04a9b5ec62f6f7f5aa45bf6a36619
Odoo 9 Development Server <hen@hiyacoding.co.uk>
2016-11-20 11:57:22 -0800

    projrct fehlt noch

--- felix1de_frontend/models/odoo/__init__.py
+++ felix1de_frontend/models/odoo/__init__.py
@@ -4,9 +4,9 @@ import res_partner
 import felix1de
 import branch
 import order
-#import customer_mail_send
+import customer_mail_send
 import customer
-#import product
-#import mandanten
-#import kontakte
-#import kanzleien
+import mandanten
+import kontakte
+import kanzleien
+#import project
